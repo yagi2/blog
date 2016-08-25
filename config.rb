d5b01fd2a8c6530db05c28163dba0e9382d08223
Itsuki Aoyagi <mail@yagi2.com>
2016-08-25 23:55:55 +0900

    blogのテンプレートでinitした

--- config.rb
+++ config.rb
@@ -1,24 +1,8 @@
 ###
-# Page options, layouts, aliases and proxies
+# Blog settings
 ###
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
+# Time.zone = "UTC"
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
@@ -26,7 +10,7 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
@@ -34,22 +18,79 @@ activate :blog do |blog|
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".md"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
   # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
+  blog.paginate = true
+  blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
+page "/sitemap.xml", layout: false
+
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
 # end
+
+# Change Compass configuration
+# config :development do
+  compass_config do |config|
+    config.sass_options = {:debug_info => true}
+  end
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Slim settings
+Slim::Engine.set_default_options :pretty => true
+# shortcut
+Slim::Engine.set_default_options :shortcut => {
+  '#' => {:tag => 'div', :attr => 'id'},
+  '.' => {:tag => 'div', :attr => 'class'},
+  '&' => {:tag => 'input', :attr => 'type'}
+}
+
+# Markdown settings 
+set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :with_toc_data => true
+set :markdown_engine, :redcarpet
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", layout: false
+#
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -58,11 +99,80 @@ page "/feed.xml", layout: false
 #   end
 # end
 
+###
+# Site Settings
+###
+# Set site setting, used in helpers / sitemap.xml / feed.xml.
+set :site_url, 'http://blog.yagi2.com'
+set :site_author, 'やぎにい'
+set :site_title, 'やぎ小屋'
+set :site_description, 'ですくりぷしょん'
+# Select the theme from bootswatch.com.
+# If false, you can get plain bootstrap style.
+# set :theme_name, 'flatly'
+set :theme_name, 'flatly'
+# set @analytics_account, like "XX-12345678-9"
+@analytics_account = false
+
+# Asset Settings
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'images'
+
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  Dir.glob(File.join("#{root}", @bower_config["directory"], "*", "fonts")) do |f|
+    sprockets.append_path f
+  end
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+
+###
+# Target settings
+###
+
+# To build the target of "android" you would run:
+# MIDDLEMAN_BUILD_TARGET=android middleman build
+
+# require 'middleman-target'
+# activate :target do |target|
+
+#  target.build_targets = {
+#    "phonegap" => {
+#      :includes => %w[android ios]
+#    }
+#  }
+
+# end
+
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
+  # For example, change the Compass output style for deployment
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
 end
+
+###
+# Deploy settings
+###
+
+# ftp deployment configuration. 
+# activate :deploy do |deploy|
+#   deploy.method = :ftp
+#   deploy.host = "ftp-host"
+#   deploy.user = "ftp-user"
+#   deploy.password = "ftp-password"
+#   deploy.path = "ftp-path"
+# end
