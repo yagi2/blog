8fc89829e1cb8db6dc765944ab3469b4b272d524
Itsuki AOYAGI <mail@yagi2.com>
2016-08-31 03:48:36 +0900

    記事ファイルの置き場所を年ごとにディレクトリ管理するように変更

--- config.rb
+++ config.rb
@@ -11,7 +11,7 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  blog.sources = "posts/{year}/{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   blog.layout = "layouts/layout"
   # blog.summary_separator = /(READMORE)/
